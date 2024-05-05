7e7e6ac63e327933811f8ace96f2d224b49d7a92
ZedDevStuff <kouamejuniorauguste@gmail.com>
2024-05-05 16:44:31 +0000

    v1.0.0

---
+++ ModMenu.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ModList
+{
+    public class ModMenu : MonoBehaviour
+    {
+        private Image Icon;
+        private TMP_Text ModName, Author, Version, Description;
+        private Button ExitButton, OpenWebsiteButton, OpenThunderstoreButton;
+        private TMP_InputField Searchbox;
+        private ScrollRect Mods;
+        private List<KeyValuePair<ModEntry, GameObject>> AllModsObjects = new();
+        private List<KeyValuePair<ModEntry, GameObject>> FilteredMods = new();
+        private ModEntry CurrentMod;
+        public void SetCurrentMod(ModEntry mod)
+        {
+            CurrentMod = mod;
+            Icon.sprite = mod.Icon;
+            ModName.text = mod.Name;
+            Author.text = "by " + mod.Author;
+            Version.text = "v" + mod.BepInExVersion;
+            Description.text = mod.Description;
+            if(mod.IsFromThunderstore) OpenThunderstoreButton.gameObject.SetActive(true);
+            else OpenThunderstoreButton.gameObject.SetActive(false);
+            if(mod.AuthorWebsite != "") OpenWebsiteButton.gameObject.SetActive(true);
+            else OpenWebsiteButton.gameObject.SetActive(false);
+        }
+        public void SetCurrentMod(GameObject g)
+        {
+            SetCurrentMod(AllModsObjects.Where(m => m.Value == g).First().Key);
+        }
+        void Awake()
+        {
+            Icon = transform.Find("Container/Mod/Viewport/Content/ModInfo/Icon").GetComponent<Image>();
+            ModName = transform.Find("Container/Mod/Viewport/Content/ModInfo/Icon/ModName").GetComponent<TMP_Text>();
+            Version = transform.Find("Container/Mod/Viewport/Content/ModInfo/Icon/Version").GetComponent<TMP_Text>();
+            Author = transform.Find("Container/Mod/Viewport/Content/ModInfo/Icon/Author").GetComponent<TMP_Text>();
+            Description = transform.Find("Container/Mod/Viewport/Content/Description").GetComponent<TMP_Text>();
+            OpenWebsiteButton = transform.Find("Container/Mod/Viewport/Content/Buttons/OpenWebsite").GetComponent<Button>();
+            OpenWebsiteButton.onClick.AddListener(() => Application.OpenURL(CurrentMod.AuthorWebsite));
+            OpenWebsiteButton.gameObject.SetActive(false);
+            OpenThunderstoreButton = transform.Find("Container/Mod/Viewport/Content/Buttons/ThunderstorePage").GetComponent<Button>();
+            OpenThunderstoreButton.onClick.AddListener(() => Application.OpenURL(CurrentMod.ThunderstoreUrl));
+            OpenThunderstoreButton.gameObject.SetActive(false);
+            ExitButton = transform.Find("Exit").GetComponent<Button>();
+            ExitButton.onClick.AddListener(() => Destroy(gameObject));
+            Searchbox = transform.Find("SearchBox").GetComponent<TMP_InputField>();
+            Searchbox.onValueChanged.AddListener(Search);
+            Mods = transform.Find("Container/Mods").GetComponent<ScrollRect>();
+
+            foreach(ModEntry mod in Plugin.Mods)
+            {
+                GameObject card = Instantiate(Plugin.ModCardPrefab, Mods.content);
+                card.AddComponent<Button>().onClick.AddListener(() => SetCurrentMod(card));
+                AllModsObjects.Add(new(mod, card));
+                card.transform.Find("Icon").GetComponent<Image>().sprite = mod.Icon;
+                card.transform.Find("Icon/ModName").GetComponent<TMP_Text>().text = mod.Name;
+                card.transform.Find("Icon/Version").GetComponent<TMP_Text>().text = "v" + mod.BepInExVersion;
+                card.transform.Find("Icon/Author").GetComponent<TMP_Text>().text = "by " + mod.Author;
+            }
+            SetCurrentMod(AllModsObjects.First().Key);
+        }
+        public void Search(string search)
+        {
+            if(search == "")
+            {
+                foreach(var kvp in AllModsObjects)
+                {
+                    kvp.Value.SetActive(true);
+                }
+            }
+            FilteredMods = AllModsObjects.Where(m => m.Key.Name.Contains(search)).ToList();
+            foreach(var kvp in AllModsObjects)
+            {
+                if(!FilteredMods.Contains(kvp)) kvp.Value.SetActive(false); 
+                else kvp.Value.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,144 @@
+﻿using BepInEx;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using ModList;
+using Mono.Cecil;
+using MonoMod.Utils;
+using Newtonsoft.Json.Linq;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+namespace ModList
+{
+    [BepInPlugin("com.zeddevstuff.modlist", "ModList", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static List<ModEntry> Mods = new();
+        private static AssetBundle Bundle;
+        internal static GameObject ModmenuPrefab, ModMenu, ModCardPrefab, ModsButton;
+        private void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        private void Start()
+        {
+            if(Mods.Count != 0) return;
+            var plugins = gameObject.GetComponents<BaseUnityPlugin>();
+            Bundle = AssetBundle.LoadFromMemory(Resource1.modlist);
+            ModmenuPrefab = Bundle.LoadAsset<GameObject>("ModMenu");
+            ModCardPrefab = Bundle.LoadAsset<GameObject>("ModCard");
+            ModsButton = Bundle.LoadAsset<GameObject>("ModsButton");
+            foreach(var plugin in plugins)
+            {
+                AssemblyDescriptionAttribute desc = plugin.GetType().Assembly.GetCustomAttribute<AssemblyDescriptionAttribute>();
+                ModEntry entry = new();
+                entry.Name = plugin.Info.Metadata.Name;
+                entry.BepInExGuid = plugin.Info.Metadata.GUID;
+                entry.BepInExVersion = plugin.Info.Metadata.Version.ToString();
+                string[] deconstructedGuid = plugin.Info.Metadata.GUID.Split('.');
+                entry.Author = deconstructedGuid.Length >= 2 ? deconstructedGuid[1] : "Unknown Author";
+                entry.Description = IsStringWhiteSpaceOrEmpty(desc.Description) ? "No Description" : desc.Description;
+                DirectoryInfo? dir = new FileInfo(plugin.GetType().Assembly.Location).Directory;
+                if(dir != null)
+                {
+                    if(!dir.Name.Contains("plugins"))
+                    {
+                        FileInfo? manifestFile = dir.GetFiles("manifest.json").FirstOrDefault();
+                        if(manifestFile != null)
+                        {
+                            JObject? manifest = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(manifestFile.FullName));
+                            if(manifest != null)
+                            {
+                                FileInfo? icon = manifestFile.Directory.GetFiles("icon.png").FirstOrDefault();
+                                Texture2D tex = new(256, 256);
+                                if (icon != null) tex.LoadImage(File.ReadAllBytes(icon.FullName));
+                                else tex.LoadImage(Resource1.noicon);
+                                entry.Icon = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new(0.5f, 0.5f));
+                                entry.IsFromThunderstore = true;
+                                if(manifest.GetValue("description") != null)
+                                {
+                                    entry.Description = manifest.GetValue("description")!.ToString();
+                                }
+                                string modName = manifest.GetValue("name")?.ToString() ?? "";
+                                if(modName != "") entry.Name = modName;
+                                entry.ThunderstoreVersion = manifest.GetValue("version_number")?.ToString() ?? "";
+                                string url = manifest.GetValue("website_url")?.ToString() ?? "";
+                                entry.AuthorWebsite = url != "" ? url : "";
+                                string author = manifestFile.Directory.Name.Replace("-" + modName, "");
+                                entry.ThunderstoreUrl = $"https://thunderstore.io/c/ultrakill/p/{author}/{modName}";
+                                entry.ThunderstoreGuid = manifestFile.Directory.Name;
+                                entry.Author = author;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Texture2D tex = new(256, 256);
+                        tex.LoadImage(Resource1.noicon);
+                        entry.Icon = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new(0.5f, 0.5f));
+                    }
+                }
+                Mods.Add(entry);
+            }
+            Mods = Mods.OrderBy(m => m.Name).ToList();
+            SceneManager.sceneLoaded += AddButton;
+        }
+
+        private void AddButton(Scene s, LoadSceneMode mode)
+        {
+            if(SceneHelper.CurrentScene.Contains("Menu"))
+            {
+                GameObject canvas = s.GetRootGameObjects().Where(c => c.name == "Canvas").First();
+                if(canvas.transform.Find("ModsButton") == null)
+                {
+                    GameObject button = Instantiate(ModsButton, canvas.GetComponent<RectTransform>());
+                    button.GetComponentInChildren<Button>().onClick.AddListener(() => OpenModMenu());
+                    RectTransform rect = button.GetComponent<RectTransform>();
+                    rect.SetPivot(PivotPresets.BottomLeft);
+                    rect.SetAnchor(AnchorPresets.BottomLeft);
+                    rect.anchoredPosition = new Vector2(5, 5);
+                }
+            }
+        }
+        private void OpenModMenu()
+        {
+            RectTransform canvas = GameObject.Find("Canvas").GetComponent<RectTransform>();
+            Instantiate(ModmenuPrefab, canvas).AddComponent<ModMenu>();
+        }
+        public static T AorB<T>(T a, T b, Func<bool> condition)
+        {
+            return condition() ? a : b;
+        }
+        private static bool IsStringWhiteSpaceOrEmpty(string str)
+        {
+            return str.Length == 0 || str.All(c => char.IsWhiteSpace(c));
+        }
+       
+    }
+    public struct ModEntry
+    {
+        public Sprite? Icon;
+        public string Name = "";
+        public string Author = "";
+        public string BepInExGuid = "";
+        public string ThunderstoreGuid = "";
+        public string BepInExVersion = "";
+        public string ThunderstoreVersion = "";
+        public string Description = "";
+        public string AuthorWebsite = "";
+        public string ThunderstoreUrl = "";
+        public bool IsFromThunderstore = false;
+
+        public ModEntry()
+        {
+
+        }
+    }
+    
+}
